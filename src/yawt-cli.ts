e493e985fa6bd5c541535d4ffeec2cf2aa1412a3
aegenet <20248096+aegenet@users.noreply.github.com>
2024-04-18 21:41:06 +0200

    fix(yawt-cli): env

--- src/yawt-cli.ts
+++ src/yawt-cli.ts
@@ -1,5 +1,6 @@
 import { type YawtTaskNames, yawt } from './yawt';
 import { argv2Object } from './utils/argv-2-object';
+import { env, argv, exit } from 'node:process';
 
 const cliParams = argv2Object<{
   task: YawtTaskNames;
@@ -11,13 +12,13 @@ const cliParams = argv2Object<{
   npmNamespace: string;
   publish: boolean;
   npmVersion: string;
-}>(process.argv.slice(2));
+}>(argv.slice(2));
 
 let npmVersion: string | undefined = cliParams.npmVersion?.trim();
 if (!npmVersion) {
-  if (process.env.GITHUB_REF_TYPE === 'tag') {
-    npmVersion = process.env.GITHUB_REF_NAME;
-  } else if (process.env.GITHUB_REF_NAME) {
+  if (env.GITHUB_REF_TYPE === 'tag') {
+    npmVersion = env.GITHUB_REF_NAME;
+  } else if (env.GITHUB_REF_NAME) {
     // workflow github
     npmVersion = `0.${new Date().getTime()}.0-dev`;
   }
@@ -25,18 +26,18 @@ if (!npmVersion) {
 
 yawt({
   task: cliParams.task,
-  workers: cliParams.workers || parseInt(process.env.YAWT_WORKER_THREADS || '8', 10),
-  silent: cliParams.silent || process.env.YAWT_SILENT === 'true',
-  single: cliParams.single || process.env.YAWT_SINGLE === 'true',
-  npmRegistryURL: cliParams.npmRegistryURL || process.env.YAWT_NPM_PUSH_REGISTRY,
-  npmPublicPublish: cliParams.npmPublicPublish || process.env.YAWT_NPM_PUBLIC_PUBLISH === 'true',
-  npmNamespace: cliParams.npmNamespace || process.env.YAWT_NPM_NAMESPACE,
+  workers: cliParams.workers || parseInt(env.YAWT_WORKER_THREADS || '8', 10),
+  silent: cliParams.silent || env.YAWT_SILENT === 'true',
+  single: cliParams.single || env.YAWT_SINGLE === 'true',
+  npmRegistryURL: cliParams.npmRegistryURL || env.YAWT_NPM_PUSH_REGISTRY,
+  npmPublicPublish: cliParams.npmPublicPublish || env.YAWT_NPM_PUBLIC_PUBLISH === 'true',
+  npmNamespace: cliParams.npmNamespace || env.YAWT_NPM_NAMESPACE,
   npmVersion: npmVersion,
 })
   .then(() => {
-    process.exit(0);
+    exit(0);
   })
   .catch(e => {
     console.error(e);
-    process.exit(1);
+    exit(1);
   });
