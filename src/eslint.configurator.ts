ed7e1ce361d7b8724f3c6392542e04e428fa23b9
aegenet <20248096+aegenet@users.noreply.github.com>
2024-05-15 23:23:17 +0200

    chore(eslint): eslint 9, typescript-eslint 8 alpha (compat eslint  9)

--- src/eslint.configurator.ts
+++ src/eslint.configurator.ts
@@ -1,8 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+import prettier from 'eslint-plugin-prettier';
 import type { rules as eslintRules } from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 
@@ -22,94 +20,108 @@ export function eslintConfigurator({
   namingConvention?: boolean;
   rules?: typeof eslintRules;
 } = {}) {
-  return tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettierRecommended, {
-    // files: ['*.ts', '*.tsx'],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: 'module',
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: cwd,
+  return tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+      plugins: {
+        // others
+        prettier: prettier,
       },
-      globals: {
-        ...(type === 'node' ? { ...globals.node, ...globals.nodeBuiltin } : globals.browser),
-        ...globals.es2021,
+      rules: {
+        // others
+        ...(prettier.configs!.recommended as any).rules,
       },
     },
-    rules: {
-      'no-console': noConsole ? 'error' : 'warn',
-      'prettier/prettier': ['error'],
-      '@typescript-eslint/no-inferrable-types': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': ['error'],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { args: 'after-used', ignoreRestSiblings: true }],
-      '@typescript-eslint/naming-convention': namingConvention
-        ? [
-            'error',
-            // function
-            {
-              selector: 'function',
-              modifiers: ['exported'],
-              format: ['camelCase'],
-              leadingUnderscore: 'forbid',
-            },
-            {
-              selector: 'function',
-              format: ['camelCase'],
-              leadingUnderscore: 'require',
-            },
-            // Classes properties and methods
-            {
-              selector: ['classProperty', 'classMethod'],
-              modifiers: ['public'],
-              format: ['camelCase'],
-              leadingUnderscore: 'forbid',
-            },
-            {
-              selector: ['classProperty', 'classMethod'],
-              modifiers: ['private'],
-              format: ['camelCase'],
-              leadingUnderscore: 'require',
-            },
-            // static public/private
-            {
-              selector: ['classProperty'],
-              modifiers: ['static', 'private'],
-              format: ['UPPER_CASE'],
-              leadingUnderscore: 'require',
-            },
-            {
-              selector: ['method'],
-              modifiers: ['static', 'private'],
-              format: ['camelCase'],
-              leadingUnderscore: 'require',
-            },
-            {
-              selector: ['classProperty'],
-              modifiers: ['static', 'public'],
-              format: ['UPPER_CASE'],
-              leadingUnderscore: 'require',
-            },
-            {
-              selector: ['method'],
-              modifiers: ['static', 'public'],
-              format: ['camelCase'],
-              leadingUnderscore: 'forbid',
-            },
-          ]
-        : ['off'],
-      // '@typescript-eslint/explicit-member-accessibility': [
-      //   'error',
-      //   {
-      //     accessibility: 'explicit',
-      //     overrides: {
-      //       constructors: 'no-public',
-      //     },
-      //   },
-      // ],
-      ...rules,
-    },
-  });
+    {
+      // files: ['*.ts', '*.tsx'],
+      languageOptions: {
+        ecmaVersion: 2023,
+        sourceType: 'module',
+        parserOptions: {
+          project: './tsconfig.json',
+          tsconfigRootDir: cwd,
+        },
+        globals: {
+          ...(type === 'node' ? { ...globals.node, ...globals.nodeBuiltin } : globals.browser),
+          ...globals.es2021,
+        },
+      },
+      rules: {
+        'no-console': noConsole ? 'error' : 'warn',
+        'prettier/prettier': ['error'],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-floating-promises': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { args: 'after-used', ignoreRestSiblings: true }],
+        '@typescript-eslint/naming-convention': namingConvention
+          ? [
+              'error',
+              // function
+              {
+                selector: 'function',
+                modifiers: ['exported'],
+                format: ['camelCase'],
+                leadingUnderscore: 'forbid',
+              },
+              {
+                selector: 'function',
+                format: ['camelCase'],
+                leadingUnderscore: 'require',
+              },
+              // Classes properties and methods
+              {
+                selector: ['classProperty', 'classMethod'],
+                modifiers: ['public'],
+                format: ['camelCase'],
+                leadingUnderscore: 'forbid',
+              },
+              {
+                selector: ['classProperty', 'classMethod'],
+                modifiers: ['private'],
+                format: ['camelCase'],
+                leadingUnderscore: 'require',
+              },
+              // static public/private
+              {
+                selector: ['classProperty'],
+                modifiers: ['static', 'private'],
+                format: ['UPPER_CASE'],
+                leadingUnderscore: 'require',
+              },
+              {
+                selector: ['method'],
+                modifiers: ['static', 'private'],
+                format: ['camelCase'],
+                leadingUnderscore: 'require',
+              },
+              {
+                selector: ['classProperty'],
+                modifiers: ['static', 'public'],
+                format: ['UPPER_CASE'],
+                leadingUnderscore: 'require',
+              },
+              {
+                selector: ['method'],
+                modifiers: ['static', 'public'],
+                format: ['camelCase'],
+                leadingUnderscore: 'forbid',
+              },
+            ]
+          : ['off'],
+        // '@typescript-eslint/explicit-member-accessibility': [
+        //   'error',
+        //   {
+        //     accessibility: 'explicit',
+        //     overrides: {
+        //       constructors: 'no-public',
+        //     },
+        //   },
+        // ],
+        ...rules,
+      },
+    }
+  );
 }
